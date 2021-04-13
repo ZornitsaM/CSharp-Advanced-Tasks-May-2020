5516d1dfc5b750e91a9727311c9c83c0c78adef7
Zornitsa Muletarova <79112032+ZMuletarova@users.noreply.github.com>
2021-04-13 21:50:13 +0300

    add

---
+++ CSharp Final Exam/Exam - 28.06.2020 - MyExam ; )/Exam/PO1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputBombEffect = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var inputBombCasing = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Stack<int> bombEffect = new Stack<int>(inputBombEffect.Reverse());
+            Stack<int> bombCasing = new Stack<int>(inputBombCasing);
+
+            int daturaBombs = 40;
+            int cherryBombs = 60;
+            int smokeDecoyBombs = 120;
+
+            int daturaCrafted = 0;
+            int cherryCrafted = 0;
+            int smokeCrafted = 0;
+
+            bool isThreeCrafted = false;
+
+
+            while (true)
+            {
+                if (bombEffect.Count == 0 || bombCasing.Count == 0)
+                {
+                    break;
+                }
+
+                if (daturaCrafted >= 3 && cherryCrafted >= 3 && smokeCrafted >= 3)
+                {
+                    isThreeCrafted = true;
+                    break;
+
+                }
+
+
+                int currentEffect = bombEffect.Peek();
+                int currentCasing = bombCasing.Peek();
+                int sum = currentCasing + currentEffect;
+
+                if (sum == daturaBombs)
+                {
+                    daturaCrafted++;
+                    bombEffect.Pop();
+                    bombCasing.Pop();
+                }
+
+                else if (sum == cherryBombs)
+                {
+                    cherryCrafted++;
+                    bombEffect.Pop();
+                    bombCasing.Pop();
+                }
+
+                else if (sum == smokeDecoyBombs)
+                {
+                    smokeCrafted++;
+                    bombEffect.Pop();
+                    bombCasing.Pop();
+                }
+
+
+                else
+                {
+                    currentCasing -= 5;
+                    bombCasing.Pop();
+                    bombCasing.Push(currentCasing);
+
+                }
+
+
+            }
+
+            if (isThreeCrafted)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+
+
+            if (bombEffect.Count > 0)
+            {
+                Console.WriteLine($"Bomb Effects: {string.Join(", ", bombEffect)}");
+            }
+
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+
+            if (bombCasing.Count > 0)
+            {
+                Console.WriteLine($"Bomb Casings: {string.Join(", ", bombCasing)}");
+            }
+
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+
+
+            Console.WriteLine($"Cherry Bombs: {cherryCrafted}");
+
+            Console.WriteLine($"Datura Bombs: {daturaCrafted}");
+
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeCrafted}");
+
+
+
+
+
+
+        }
+    }
+}
---
+++ CSharp Final Exam/Exam - 28.06.2020 - MyExam ; )/Exam/Parking/Parking.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+
+        private string type;
+        private int capacity;
+        private List<Car> data;
+
+        public Parking(string type, int capacity)
+        {
+            this.data = new List<Car>();
+            this.Type = type;
+            this.Capacity = capacity;
+        }
+
+        public int Count
+        {
+            get { return this.data.Count; }
+            
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public void Add(Car car)
+        {
+            if (this.data.Count<this.Capacity)
+            {
+                this.data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Car curentCar = this.data.Where(x => x.Manufacturer == manufacturer)
+                .FirstOrDefault(x => x.Model == model);
+
+            if (curentCar != null)
+            {
+                this.data.Remove(curentCar);
+                return true;
+            }
+            return false;
+        }
+
+
+        public Car GetLatestCar()
+        {
+            if (this.data.Count == 0)
+            {
+                return null;
+            }
+
+            else
+            {
+                Car latestCar = this.data.OrderByDescending(x => x.Year).FirstOrDefault();
+                return latestCar;
+            }
+        }
+
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car currentCar = this.data.Where(x => x.Manufacturer == manufacturer)
+                .FirstOrDefault(x => x.Model == model);
+
+            if (currentCar!=null)
+            {
+                return currentCar;
+            }
+
+            return null;
+        }
+
+
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {this.Type}:");
+
+            foreach (var item in this.data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
